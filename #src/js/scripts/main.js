35e671c0d2e99fb80e013d725caa4381960c8cc0
colocolov <colocolov.stas@gmail.com>
2022-12-19 11:53:47 +0200

    animate count in sec advantages

--- #src/js/scripts/main.js
+++ #src/js/scripts/main.js
@@ -17,20 +17,46 @@ function showContent() {
   mainContent.classList.add("_active");
 }
 
-const dataCounters = document.querySelectorAll(".advantages__name");
+// scroll
+const isInViewport = function (elem) {
+  const distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
 
-dataCounters.forEach((item) => {
-  item.innerHTML = "0";
-  const updateCounter = () => {
-    const target = +item.getAttribute("data-counter");
-    const c = +item.innerText;
+const findMe = document.getElementById("advantages__container");
+let eventStatus = false;
 
-    if (c < target) {
-      item.innerText = c + 1;
-      setTimeout(updateCounter, 1);
-    } else {
-      item.innerText = target;
+window.addEventListener(
+  "scroll",
+  function (e) {
+    if (isInViewport(findMe)) {
+      if (!eventStatus) {
+        // animate count scriprt
+        const dataCounters = document.querySelectorAll(".advantages__name");
+        let interval = 1500;
+
+        dataCounters.forEach((item) => {
+          let startValue = 0;
+          let endValue = parseInt(item.getAttribute("data-counter"));
+          let duration = Math.floor(interval / endValue);
+          let counter = setInterval(function () {
+            startValue += 1;
+            item.textContent = startValue;
+            if (startValue == endValue) {
+              clearInterval(counter);
+            }
+          }, duration);
+        });
+        eventStatus = true;
+      }
     }
-  };
-  updateCounter();
-});
+  },
+  false
+);
